38b44f998455ea175036bee4c1edb1ff88ef8c6e
jr2339 <jr2339@nau.edu>
2018-07-17 14:57:48 -0700

    New Content

--- AngularJS/1.js
+++ AngularJS/1.js
@@ -1,8 +1,8 @@
-<h2 align="center">What is AngularJS?</h3>
-<h4>AngularJS is a JavaScript framework that helps build web applications.</h4></br>
-<h4 color="yellow">Benefits of Angulars</h4>
-<h5>Dependency Injction
-<h5>Two Way Data-Binding
+				What is AngularJS?
+AngularJS is a JavaScript framework that helps build web applications.
+Benefits of Angulars
+Dependency Injction
+Two Way Data-Binding
 		 ---------------
 		|		|
 	--------|     Model     |<----------
@@ -22,7 +22,7 @@
 	-------->|      View      |---------
 		 |		  |
 		  ----------------
-<h5>Testing
-<h5> Model View Controller
-<h5> Directives, Filters, etc...
+Testing
+Model View Controller
+Directives, Filters, etc...
 
