346d66859d8b9e6168935ab06a6cadca7e1d5ea8
jr2339 <jr2339@nau.edu>
2018-07-17 14:48:07 -0700

    New Content

---
+++ AngularJS/1.js
@@ -0,0 +1,28 @@
+<h2 align="center">What is AngularJS?</h3>
+<h4>AngularJS is a JavaScript framework that helps build web applications.</h4></br>
+<h4 color="yellow">Benefits of Angulars</h4>
+<h5>Dependency Injction
+<h5>Two Way Data-Binding
+		 ---------------
+		|		|
+	--------|     Model     |<----------
+	|	|		|	    |
+	|	 ---------------	    |
+	|				    |
+	|				    |
+	|				    |
+	|Change	to Model	            | Change to the View	
+	|				    |
+	|				    |
+	|                                   |
+	|				    |
+	|	 			    |
+	|	  ----------------	    |
+	|	 |		  |	    |
+	-------->|      View      |---------
+		 |		  |
+		  ----------------
+<h5>Testing
+<h5> Model View Controller
+<h5> Directives, Filters, etc...
+
