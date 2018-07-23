0ded35e9b2001abe0aa03bd7eed839f16069b693
jr2339 <jr2339@nau.edu>
2018-07-23 15:00:45 -0700

    Two way data binding

---
+++ AngularJS/5.js
@@ -0,0 +1,31 @@
+///Create the Module
+var myApp = angular.module("myModule",[]);
+
+/// Create the controller
+var myController = function($scope){
+	
+	var employee= {
+		firstName: "Ben",
+		lastName: "Green",
+		gender:"Male"
+	};
+
+
+	$scope.employee = employee;
+};
+
+///Register the controller with the module
+myApp.controller("myController", myController);
+
+
+/**
+or we can do such as this
+
+Creating the controller and registering with the module all done in one line
+
+myApp.controller("myController", function($scope){
+	$scope.message = "AngularJS Tutorial";
+});
+**/
+
+
